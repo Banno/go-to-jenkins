763d86d11126e66b9f2d6d5b65eb7702b179cefe
Brian Stokes <bsstokes@gmail.com>
2019-02-25 17:29:48 -0600

    Format

--- content.js
+++ content.js
@@ -1,11 +1,23 @@
 function getJenkinsUrl(pathname) {
-    var urlMap = [
-        { regexp: new RegExp("/Banno/mobile-android/pull/"), url: "https://jenkins.dev.banno-internal.com/job/mobile-android/job/android-tests/view/change-requests/job/PR-" },
-        { regexp: new RegExp("/Banno/conversations-android/pull/"), url: "https://jenkins.dev.banno-internal.com/job/mobile-android/job/conversations/job/tests/view/change-requests/job/PR-" },
-        { regexp: new RegExp("/Banno/zelle-android/pull/"), url: "https://jenkins.dev.banno-internal.com/job/mobile-android/job/zelle/job/tests/view/change-requests/job/PR-" }
+    var urls = [
+        {
+            regexp: new RegExp("/Banno/mobile-android/pull/"),
+            url: "https://jenkins.dev.banno-internal.com/job/mobile-android/job/android-tests/view/change-requests/job/PR-"
+        },
+        {
+            regexp: new RegExp("/Banno/conversations-android/pull/"),
+            url: "https://jenkins.dev.banno-internal.com/job/mobile-android/job/conversations/job/tests/view/change-requests/job/PR-"
+        },
+        {
+            regexp: new RegExp("/Banno/zelle-android/pull/"),
+            url: "https://jenkins.dev.banno-internal.com/job/mobile-android/job/zelle/job/tests/view/change-requests/job/PR-"
+        }
     ]
 
-    return urlMap.filter(x => x.regexp.test(pathname)).map(x => pathname.replace(x.regexp, x.url)).find(x => true)
+    return urls
+        .filter(x => x.regexp.test(pathname))
+        .map(x => pathname.replace(x.regexp, x.url))
+        .find(x => true)
 }
 
 function addButtonIfNecessary() {
